f749ea54b3dcd66d719d6613d51bb6551b118682
Ronyan Lucietti <ronyan_lucietti@hotmail.com>
2017-11-17 17:14:54 -0200

    js cap5

--- introducao-javascript/js/calcula-imc.js
+++ introducao-javascript/js/calcula-imc.js
@@ -27,11 +27,17 @@ for(var i = 0; i < pacientes.length; i++){
     }
     
     if (alturaValida && pesoValido){
-        var imc = peso / (altura * altura);
-        tdImc.textContent = imc.toFixed(2);
+        var imc = calculaImc(peso,altura);
+        tdImc.textContent = imc;
     }else{
         paciente.classList.add("paciente-invalido");
     }
 }
 
+function calculaImc(peso,altura){
+    var imc = 0;
 
+    imc = peso / (altura * altura);
+
+    return imc.toFixed(2);
+}
